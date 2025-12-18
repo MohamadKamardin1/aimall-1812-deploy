6b15690442cc1c6afba39a71e40c739c848d34d9
Mohamad Kamardin <188096203+MohamadKamardin1@users.noreply.github.com>
2025-12-18 06:19:12 +0300

    create a superuser

--- render-build.sh
+++ render-build.sh
@@ -15,3 +15,20 @@ pip install pillow --no-cache-dir
 # Static files and migrations
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+phone = "+255778769590"
+password = "1234"
+if not User.objects.filter(phone_number=phone, user_type="admin").exists():
+    User.objects.create_superuser(
+        phone_number=phone,
+        password=password,
+        user_type="admin",
+        is_staff=True,
+        is_superuser=True,
+        is_active=True
+    )
+EOF
